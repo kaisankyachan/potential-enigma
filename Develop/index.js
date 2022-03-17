e0a5ae427f78a2f8209dc6b4b81656549f276658
kaisankyachan <kaisankyachan@gmail.com>
2022-03-16 19:45:18 -0600

    initialize git

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+var fs = require("fs");
+var readMe = "README.md"
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Title of project: ",
+    "Description of project: "
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Readme generated!");
+    }); 
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {  
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "title",
+                message: questions[0]
+            },
+            {
+                type: "input",
+                name: "desc",
+                message: questions[1]
+            }
+        ])
+        .then((answers) => {
+            var format = `# ${answers.title}\n` + `${answers.desc}\n`
+            writeToFile(readMe, format)
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+    
+}
 
 // Function call to initialize app
 init();
